efe493a512590945c29c2f9d1db141169453eb92
se0875 <se0875@sz.vtech>
2020-06-10 16:01:03 +0800

    propType use

--- demo01/src/com.js
+++ demo01/src/com.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Testcomponent extends Component {
     constructor(props) {
@@ -8,13 +9,24 @@ class Testcomponent extends Component {
     }
     render() { 
         return (  
-            <li onClick={this.handleClick}>{this.props.content}</li>
+        <li onClick={this.handleClick}>{this.props.avaname}为你加油-{this.props.content}</li>
         );
     }
     handleClick() {
         this.props.list = []
         this.props.onClickDelItem(this.props.index)
     }
+}
+
+Testcomponent.propTypes = {
+    avaname: PropTypes.string.isRequired,
+    content: PropTypes.string,
+    index: PropTypes.number,
+    onClickDelItem: PropTypes.func
+}
+
+Testcomponent.defaultProps = {
+    avaname: 'Ben'
 }
  
 export default Testcomponent;
